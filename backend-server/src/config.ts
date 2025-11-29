bc7cadc6e2810311ae425187d74aaf934df6310c
ParthG1810 <parthg1810@gmail.com>
2025-11-29 01:43:01 -0400

    Revert "Convert application to Electron desktop app"

--- backend-server/src/config.ts
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const HOST_API = process.env.HOST_API || 'http://localhost:3000';
-
-export const DEV_API = 'http://localhost:3000';
-
-export const PRODUCTION_API = 'http://localhost:3000';
